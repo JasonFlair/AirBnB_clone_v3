59323efcca1813b53eee7c0ccfe2b5dfe5b4f9a1
JasonFlair <emelieobumse100@gmail.com>
2023-01-20 19:50:21 +0100

    ch

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """blueprint index?"""
-from api.v1.views import app_views
+import api.v1.views
 from json import jsonify
+
+app_views = api.v1.views.app_views
 
 
 @app_views.route("/status")
