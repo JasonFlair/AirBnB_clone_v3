876addb992a900ae9a2b392cf96b3e7cac4a16ad
JasonFlair <emelieobumse100@gmail.com>
2023-01-21 10:15:41 +0100

    check

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -14,3 +14,14 @@ def get_states():
         state_dict = state.to_dict()
         results.append(state_dict)
     return jsonify(results)
+
+
+@app_views.route("/states/<state_id>", strict_slashes=False)
+def get_one_state(state_id):
+    all_states = storage.all(State).values()
+    result = []
+    for state in all_states:
+        state_dict = state.to_dict()
+        if state_dict.id == state_id:
+            result.append(state_dict)
+    return jsonify(result)
