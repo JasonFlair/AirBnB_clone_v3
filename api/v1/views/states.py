bc1cf403ab47d5f3c3f5443e0b8dae9fe7148b74
Jason Flair <emelieobumse100@gmail.com>
2023-01-21 10:04:18 +0100

    g

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """blueprint index?"""
 from api.v1.views import app_views
-from flask import jsonify, strict_slashes
+from flask import jsonify
 from models.state import State
 from models import storage
 
 
 @app_views.route("/states", strict_slashes=False)
-def get_states(states):
-    results = storage.all(State)
-    print(results)
+def get_states():
+    results = storage.all(State).to_dict()
+    print(jsonify(results))
