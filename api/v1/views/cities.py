7b6080f238cb257fbc576ec4493f1b8f920a450d
JasonFlair <emelieobumse100@gmail.com>
2023-01-21 13:43:23 +0100

    new city fix

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -14,11 +14,6 @@ def get_cities(state_id):
     if not state:
         abort(404)
     return jsonify(state.cities)
-    """list_of_cities = []
-    for city in state.cities:
-        list_of_cities.append(city.to_dict())
-    return jsonify(list_of_cities)
-    """
 
 
 @app_views.route("/states/<state_id>", strict_slashes=False)
