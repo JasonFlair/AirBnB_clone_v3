d778215db0fb6365f74f7dd4ddaf5eec9f94f0ab
JasonFlair <emelieobumse100@gmail.com>
2023-01-21 10:09:07 +0100

    f

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -8,5 +8,9 @@ from models import storage
 
 @app_views.route("/states", strict_slashes=False)
 def get_states():
-    results = storage.all(State).to_dict()
+    all_states = storage.all(State).values()
+    results = []
+    for state in all_states:
+        state_dict = state.to_dict()
+        results.append(state_dict)
     print(jsonify(results))
