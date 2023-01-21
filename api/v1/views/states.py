35f255c01479ac0a47632fa2366af66a036cdb8e
JasonFlair <emelieobumse100@gmail.com>
2023-01-21 10:48:07 +0100

    try

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """blueprint index?"""
 from api.v1.views import app_views
-from flask import jsonify, request
+from flask import jsonify, request, abort
 from models.state import State
 from models import storage
 
@@ -18,19 +18,15 @@ def get_states():
 
 @app_views.route("/states/<state_id>", strict_slashes=False)
 def get_one_state(state_id):
-    try:
-        all_states = storage.all(State).values()
-        for state in all_states:
-            state_dict = state.to_dict()
-            if state_dict["id"] == state_id:
-                return jsonify(state_dict)
-    except TypeError:
-        error_dict = {"error": "Not found"}
-        if request.path.startswith('/api/'):
-            return jsonify(error_dict)
+    all_states = storage.all(State).values()
+    for state in all_states:
+        state_dict = state.to_dict()
+        if state_dict["id"] == state_id:
+            return jsonify(state_dict)
+    abort(404, "Not found")
 
 
-@app_views.route("/states/<state_id>", strict_slashes=False)
+@app_views.route("/states/<state_id>", methods=['DELETE'] strict_slashes=False)
 def delete_state(state_id):
     all_states = storage.all(State).values()
     for state in all_states:
@@ -38,3 +34,5 @@ def delete_state(state_id):
         if state_dict["id"] == state_id:
             del all_states[state]
             storage.save()
+        return jsonify({})
+    abort(404, "Not found")
